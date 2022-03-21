9db263180d27822398d57650ab379463bfbdc04d
Ryutaro-A <ryu1104.as@gmail.com>
2022-03-22 08:24:55 +0900

    032202

---
+++ main.py
@@ -0,0 +1,256 @@
+import streamlit as st
+import torch
+
+device = 'cpu'
+
+import sentencepiece as spm
+sp_ja = spm.SentencePieceProcessor()
+sp_ja.Load("trained_model.model")
+
+SRC_vocab = torch.load('SRC_vocab.pth')
+TRG_vocab = torch.load('TRG_vocab.pth')
+
+from typing import Tuple
+
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+
+
+class Encoder(nn.Module):
+    def __init__(self,
+                 input_dim: int,
+                 emb_dim: int,
+                 enc_hid_dim: int,
+                 dec_hid_dim: int,
+                 dropout: float):
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.emb_dim = emb_dim
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+        self.dropout = dropout
+
+        self.embedding = nn.Embedding(input_dim, emb_dim)
+
+        self.rnn = nn.GRU(emb_dim, enc_hid_dim, bidirectional = True)
+
+        self.fc = nn.Linear(enc_hid_dim * 2, dec_hid_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self,
+                src: Tensor) -> Tuple[Tensor]:
+
+        embedded = self.dropout(self.embedding(src))
+
+        outputs, hidden = self.rnn(embedded)
+
+        hidden = torch.tanh(self.fc(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1)))
+
+        return outputs, hidden
+
+
+class Attention(nn.Module):
+    def __init__(self,
+                 enc_hid_dim: int,
+                 dec_hid_dim: int,
+                 attn_dim: int):
+        super().__init__()
+
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+
+        self.attn_in = (enc_hid_dim * 2) + dec_hid_dim
+
+        self.attn = nn.Linear(self.attn_in, attn_dim)
+
+    def forward(self,
+                decoder_hidden: Tensor,
+                encoder_outputs: Tensor) -> Tensor:
+
+        src_len = encoder_outputs.shape[0]
+
+        repeated_decoder_hidden = decoder_hidden.unsqueeze(1).repeat(1, src_len, 1)
+
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+
+        energy = torch.tanh(self.attn(torch.cat((
+            repeated_decoder_hidden,
+            encoder_outputs),
+            dim = 2)))
+
+        attention = torch.sum(energy, dim=2)
+
+        return F.softmax(attention, dim=1)
+
+
+class Decoder(nn.Module):
+    def __init__(self,
+                 output_dim: int,
+                 emb_dim: int,
+                 enc_hid_dim: int,
+                 dec_hid_dim: int,
+                 dropout: int,
+                 attention: nn.Module):
+        super().__init__()
+
+        self.emb_dim = emb_dim
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+        self.output_dim = output_dim
+        self.dropout = dropout
+        self.attention = attention
+
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+
+        self.rnn = nn.GRU((enc_hid_dim * 2) + emb_dim, dec_hid_dim)
+
+        self.out = nn.Linear(self.attention.attn_in + emb_dim, output_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+
+    def _weighted_encoder_rep(self,
+                              decoder_hidden: Tensor,
+                              encoder_outputs: Tensor) -> Tensor:
+
+        a = self.attention(decoder_hidden, encoder_outputs)
+
+        a = a.unsqueeze(1)
+
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+
+        weighted_encoder_rep = torch.bmm(a, encoder_outputs)
+
+        weighted_encoder_rep = weighted_encoder_rep.permute(1, 0, 2)
+
+        return weighted_encoder_rep
+
+
+    def forward(self,
+                input: Tensor,
+                decoder_hidden: Tensor,
+                encoder_outputs: Tensor
+                ) -> Tuple[Tensor]:
+
+        input = input.unsqueeze(0)
+
+        embedded = self.dropout(self.embedding(input))
+
+        weighted_encoder_rep = self._weighted_encoder_rep(decoder_hidden,
+                                                          encoder_outputs)
+
+
+        rnn_input = torch.cat((embedded, weighted_encoder_rep), dim = 2)
+
+        output, decoder_hidden = self.rnn(rnn_input, decoder_hidden.unsqueeze(0))
+
+        embedded = embedded.squeeze(0)
+        output = output.squeeze(0)
+        weighted_encoder_rep = weighted_encoder_rep.squeeze(0)
+
+        output = self.out(torch.cat((output,
+                                     weighted_encoder_rep,
+                                     embedded), dim = 1))
+
+        return output, decoder_hidden.squeeze(0)
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self,
+                 encoder: nn.Module,
+                 decoder: nn.Module,
+                 device: torch.device):
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+    def forward(self,
+                src: Tensor,
+                ) -> Tensor:
+
+            
+        batch_size = src.shape[0]
+        max_len = 10
+        trg_vocab_size = self.decoder.output_dim
+
+        outputs = torch.zeros(max_len, batch_size, trg_vocab_size).to(self.device)
+
+        encoder_outputs, hidden = self.encoder(src)
+
+        
+        # first input to the decoder is the <sos> token
+        output = torch.tensor([TRG_vocab.stoi['<sos>']], device=self.device)
+
+        for t in range(1, max_len):
+            output, hidden = self.decoder(output, hidden, encoder_outputs)
+            outputs[t] = output
+            output = output.max(1)[1]
+        
+        return outputs
+
+INPUT_DIM = len(SRC_vocab)
+OUTPUT_DIM = len(TRG_vocab)
+ENC_EMB_DIM = 256
+DEC_EMB_DIM = 256
+ENC_HID_DIM = 512
+DEC_HID_DIM = 512
+ATTN_DIM = 128
+ENC_DROPOUT = 0.5
+DEC_DROPOUT = 0.5
+
+
+enc = Encoder(INPUT_DIM, ENC_EMB_DIM, ENC_HID_DIM, DEC_HID_DIM, ENC_DROPOUT)
+
+attn = Attention(ENC_HID_DIM, DEC_HID_DIM, ATTN_DIM)
+
+dec = Decoder(OUTPUT_DIM, DEC_EMB_DIM, ENC_HID_DIM, DEC_HID_DIM, DEC_DROPOUT, attn)
+
+model = Seq2Seq(enc, dec, device).to(device)
+
+model.load_state_dict(torch.load('models/gru_model_03_21_01.pth'))
+
+def sentence_to_tensor(sentence):
+    sentence = sp_ja.EncodeAsPieces(sentence)
+    return torch.tensor([SRC_vocab.stoi[st] for st in sentence], device=device).unsqueeze(1)
+
+def translate_sentence(input_tensor):
+    model.eval()
+    input_tensor = sentence_to_tensor(input_tensor)
+    with torch.no_grad():
+        pred = model(input_tensor)
+    pred_tokens = pred.argmax(2)[:,-1]
+    return ''.join([TRG_vocab.itos[p] for p in pred_tokens]).replace('▁', ' ')
+
+
+
+'''def init_weights(m: nn.Module):
+    for name, param in m.named_parameters():
+        if 'weight' in name:
+            nn.init.normal_(param.data, mean=0, std=0.01)
+        else:
+            nn.init.constant_(param.data, 0)
+
+
+model.apply(init_weights)'''
+
+
+
+def main():
+    # タイトル
+    st.title('日本語→英語　翻訳デモ')
+    # ヘッダ
+    st.info('このサイトは，言語処理100本ノック2020(Rev2)「99. 翻訳サーバの構築」の解答を目的としてのみ作成されました．\nテキストボックスに日本語を入力すると英語訳が出力されます．')
+    
+    # テキストボックスを生成
+    text = st.text_input(label='日本語を入力してください．')
+    st.write('English: ', translate_sentence(text))
+
+
+
+if __name__ == '__main__':
+    main()
